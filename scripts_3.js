70f1b3b82ff9fbee7110869eaee47f9c3497121f
lvanvilsteren <31972308+lvanvilsteren@users.noreply.github.com>
2017-11-16 18:11:39 -0500

    Add files via upload

---
+++ scripts_3.js
@@ -0,0 +1,11 @@
+function add(){
+
+var Number1 = document.getElementById("Number1").value;
+var Number2 = document.getElementById("Number2").value;
+
+var Answer = Number(Number1) + Number(Number2);
+
+
+document.getElementById('output').innerHTML = Answer;
+
+}
